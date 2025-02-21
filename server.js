0a9c3a72304f4b7d2a4bb94938f486cd6858d516
SRLABREEZE <srlabreeze@gmail.com>
2025-02-21 09:25:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🔥 GhostSpoof Backend Running on Port ${PORT}`);
+  app.get("/", (req, res) => {
+  res.send("🔥 GhostSpoof Backend is Running!");
+ 
 });
