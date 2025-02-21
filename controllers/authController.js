2ed976cc01bf9411a92bc4188510f548afef8b49
SRLABREEZE <srlabreeze@gmail.com>
2025-02-20 19:06:23 -0500

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, config.jwtSecret, { expiresIn: "1h" });
+    res.json({ token, user: { id: user._id, username: user.username } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
