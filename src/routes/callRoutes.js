027e84661c081a91118bfa98f060751447780abc
SRLABREEZE <srlabreeze@gmail.com>
2025-02-23 23:56:42 -0500

    Create callRoutes.js

---
+++ src/routes/callRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { makeCall } = require('../controllers/callController');
+
+const router = express.Router();
+
+router.post('/make-call', makeCall);
+
+module.exports = router;
